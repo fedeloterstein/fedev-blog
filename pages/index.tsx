04d2da229c2bd05af46a7ac7e9aa318ebdcf8eed
fedeloterstein <federicoloterstein@gmail.com>
2022-12-22 11:29:50 -0300

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Fedev - Blog | Home</title>
         </Head>
         <Container>
         <Script
